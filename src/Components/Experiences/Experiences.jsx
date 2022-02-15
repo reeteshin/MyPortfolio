20346796f3d12903dbe337e43504880625bee60e
reetesh meena <reeteshmeena73@gmail.com>
2022-02-15 19:09:00 +0530

    update2

---
+++ src/Components/Experiences/Experiences.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../ContextProvider/ThemeContext';
+import styles from './Experiences.module.css';
+
+const Experiences = () => {
+	const { newTheme } = useContext(ThemeContext);
+	return  null
+};
+
+export default Experiences;
