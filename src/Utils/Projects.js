21e448448959901e159e6bb9f3af0d8b061e487b
reetesh meena <reeteshmeena73@gmail.com>
2022-02-17 19:34:32 +0530

    Update Projects.js

--- src/Utils/Projects.js
+++ src/Utils/Projects.js
@@ -5,7 +5,7 @@ export const projects = [
 	{
 		img:  "https://reetesh-portfolio.netlify.app/svg/Purplle.png",
 		title: 'Purplle.com Clone',
-		des: 'Purplle.com was a team project. We worked on it for understanding and implementing similar features that Purplle website have. A team of 4 worked for 5 days',
+		des: 'Purplle.com was a team project. We worked on it for understanding and implement similar features that the Purplle website has. A team of 4 worked for 5 days',
 		demo: '',
 		live: 'https://purplle--clone.herokuapp.com/',
 		gitHub: 'https://github.com/kavya-2021/Purplle_Replica',
@@ -14,7 +14,7 @@ export const projects = [
 	{
 		img: 'https://reetesh-portfolio.netlify.app/svg/Bewakoof.png',
 		title: 'Bewakoof.com Clone',
-		des: 'Bewakoof.com is an Online Shopping site for Men and Women Clothing. We try to replicate the same features using HTML CSS And JavaScrip.',
+		des: 'Bewakoof.com is an Online Shopping Site for Men's and Women's Clothing. My team tries to replicate similar features using HTML CSS And JavaScript.',
 		demo: '',
 		live: 'https://inspiring-easley-d572d0.netlify.app/',
 		gitHub: 'https://github.com/reeteshin/bewakoof',
