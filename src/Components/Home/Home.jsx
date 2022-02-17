d308130e1bffb4f6473eac6a31a11bef654f266c
reetesh meena <reeteshmeena73@gmail.com>
2022-02-17 19:17:48 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -109,7 +109,7 @@ const Home = ({ scrollRef }) => {
 					className={styles.profileImage}
 				>
 					<img
-						src='https://avatars.githubusercontent.com/u/37933301?v=4'
+						src='https://reetesh-portfolio.netlify.app/svg/reetesh-removebg.png'
 						alt='Profile pic'
 					/>
 				</div>
