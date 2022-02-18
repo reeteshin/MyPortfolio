89288d3e272e94af7fc8ce6d558e558c429317b7
reetesh meena <reeteshmeena73@gmail.com>
2022-02-18 16:53:22 +0530

    Update Projects.js

--- src/Utils/Projects.js
+++ src/Utils/Projects.js
@@ -14,7 +14,7 @@ export const projects = [
 	{
 		img: 'https://reetesh-portfolio.netlify.app/svg/Bewakoof.png',
 		title: 'Bewakoof.com Clone',
-		des: "Bewakoof.com is an Online Shopping Site for Men's and Women's Clothing. My team tries to replicate similar (e.g Navbar, Home Page, Product Page, Cart Page, Payment Page ) features using HTML CSS And JavaScript",
+		des: "Bewakoof.com is an Online Shopping Site for Men's and Women's Clothing. My team tries to replicate similar (e.g Navbar, Home Page, Product Page, Cart Page, Payment Page, Wishlist Page) features using HTML CSS And JavaScript",
 		demo: '',
 		live: 'https://inspiring-easley-d572d0.netlify.app/',
 		gitHub: 'https://github.com/reeteshin/bewakoof',
